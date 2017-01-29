8bf69c93ad05ba84ee0d25c93cdf895796a59525
schmorrison <schmorrison@gmail.com>
2017-01-29 15:04:27 -0600

    added getuser by id endpoint for management

--- management/users.go
+++ management/users.go
@@ -67,3 +67,34 @@ func (ac *Auth0Client) UpdateUser(id string, p string) (u UserPayload, err error
 
 	return u, nil
 }
+
+func (ac *Auth0Client) GetUser(id string) (u UserPayload, err error) {
+	dmn := ac.Domain.String() + "/api/v2/users/" + id
+
+	udm, err := url.Parse(dmn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", udm.String(), strings.NewReader(""))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ac.Token.Raw))
+
+	res, err := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return u, nil
+}
